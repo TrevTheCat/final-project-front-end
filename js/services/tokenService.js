2ac7458797dbb38a4f1be0d2cdbeb7ecd2327ca1
TrevTheCat <alicegdunn@hotmail.co.uk>
2015-12-12 14:24:07 +0000

    addded services files

---
+++ js/services/tokenService.js
@@ -0,0 +1,26 @@
+angular
+  .module('worldApp')
+  .service("TokenService", TokenService);
+
+TokenService.$inject = ["$window", "jwtHelper"];
+function TokenService($window, jwtHelper) {
+
+  var self = this;
+
+  self.saveToken = function(token) {
+    $window.localStorage.setItem('token', token);
+  }
+
+  self.getToken = function() {
+    return $window.localStorage.getItem('token');
+  }
+
+  self.removeToken = function(){
+    $window.localStorage.removeItem('token');
+  }
+
+  self.getCurrentUser = function(){
+    var token = self.getToken();
+    return jwtHelper.decodeToken(token);
+  }
+}
