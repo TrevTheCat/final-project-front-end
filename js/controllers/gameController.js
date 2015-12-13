a4c670ee2bcffc4c648c2dc5b1b6ee08f0ea252e
TrevTheCat <alicegdunn@hotmail.co.uk>
2015-12-13 17:01:15 +0000

    4 countries selected

--- js/controllers/gameController.js
+++ js/controllers/gameController.js
@@ -1,24 +1,35 @@
 angular
   .module('worldApp')
-  .controller('GameController', GameController)
-  .constant('_', window._);
+  .controller('GameController', GameController);
 
-GameController.$inject = ['$http'];
-function GameController($http){
+GameController.$inject = ['$http', '$window'];
+function GameController($http, $window){
 
+  var _ = $window._;
   var self = this;
   self.data = [];
-  var self._ = _;
+
+  self.selectedCountries = [];
 
   function getData () {
     $http.get('https://restcountries.eu/rest/v1/all')
       .then(function(res){
-        console.log(res)
-        var info = res;
-
-        console.log(_.first(res))
-
+        self.data = res.data;
       });
   }
+
+  self.getCountryByName = function(name) {
+    return _.where(self.data, { name: name });
+  }
+
+  self.getRandomCountries = function() {
+    self.data = _.shuffle(self.data);
+    self.selectedCountries = _.slice(self.data, 0, 4);
+  }
+
+  self.checkWin = function() {
+    console.log('click')
+  }
+
   getData();
 }
